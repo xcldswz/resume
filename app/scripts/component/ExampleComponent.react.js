362d6692f47fc3c142ad79dd021cd341822aacb3
Fengda HUANG <h@phodal.com>
2015-04-04 23:45:39 +0800

    Add export as pdf button

--- app/scripts/component/ExampleComponent.react.js
+++ app/scripts/component/ExampleComponent.react.js
@@ -12,27 +12,30 @@ define([
 	'icons'
 ], function (React, Resume, Showdown, jsPDF, $) {
 	var converter = new Showdown.converter({ extensions: ['table', 'prettify', 'star', 'icons'] });
-	var doc = new jsPDF();
-	var elementHandler = {
-		'#main_content': function (element, renderer) {
-			return true;
-		}
-	};
-	var source = $('#main_content');
-	doc.fromHTML(
-		source,
-		15,
-		15,
-		{
-			'width': 180,'elementHandlers': elementHandler
-		});
 
-	doc.save("dataurlnewwindow.pdf");
 	var ExampleComponent = React.createClass({
+		onButtonClicked: function () {
+			var doc = new jsPDF();
+			var elementHandler = {
+				'#main_content': function (element, renderer) {
+					return true;
+				}
+			};
+			var source = $('#main_content').html();
+			doc.fromHTML(
+				source,
+				15,
+				15,
+				{
+					'width': 180,'elementHandlers': elementHandler
+				});
+			doc.save("dataurlnewwindow.pdf");
+		},
 		render: function () {
 			return (
 				<div className="pure-g">
 					<div className="pure-u-3 pure-u-md-2-3 example" >
+						<input type="button" className="pure-button pure-button-primary" onClick={this.onButtonClicked} value="export as pdf"/>
 						<div dangerouslySetInnerHTML={{ __html: converter.makeHtml(Resume) }}/>
 					</div>
 				</div>
--- app/scripts/component/IndexComponent.react.js
+++ app/scripts/component/IndexComponent.react.js
@@ -4,11 +4,13 @@ define([
 	'react',
 	'showdown',
 	'text!../data/example.md',
+	'jquery',
+	'jsPDF',
 	'table',
 	'prettify',
 	'star',
 	'icons'
-], function (React, Showdown, example) {
+], function (React, Showdown, example, $, jsPDF) {
 	var converter = new Showdown.converter({ extensions: ['table', 'prettify', 'star', 'icons'] });
 
 	var IndexComponent = React.createClass({
@@ -18,10 +20,28 @@ define([
 		handleChange: function () {
 			this.setState({value: this.refs.textarea.getDOMNode().value});
 		},
+		onButtonClicked: function () {
+			var doc = new jsPDF();
+			var elementHandler = {
+				'#main_content': function (element, renderer) {
+					return true;
+				}
+			};
+			var source = $('#main_content .resume').html();
+			doc.fromHTML(
+				source,
+				15,
+				15,
+				{
+					'width': 180,'elementHandlers': elementHandler
+				});
+			doc.save("dataurlnewwindow.pdf");
+		},
 		render: function () {
 			return (
 				<div>
 					<div className="pure-u-1 pure-u-md-1-2">
+						<input type="button" className="pure-button pure-button-primary" onClick={this.onButtonClicked} value="export as pdf"/>
 						<h3>Input</h3>
 						<textarea
 							onChange={this.handleChange}
