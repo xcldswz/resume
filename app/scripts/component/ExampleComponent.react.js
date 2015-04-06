6ef1d645e1543c7b0a1bae909bd87cf1abe68d50
Fengda HUANG <h@phodal.com>
2015-04-06 13:39:48 +0800

    use keyword for search

--- app/scripts/component/ExampleComponent.react.js
+++ app/scripts/component/ExampleComponent.react.js
@@ -11,42 +11,25 @@ define([
 	'star',
 	'icons'
 ], function (React, Resume, Showdown, jsPDF, $) {
-	var converter = new Showdown.converter({ extensions: ['table', 'prettify', 'star', 'icons'] });
+	var converter, html, ExampleComponent;
+	converter = new Showdown.converter({extensions: ['table', 'prettify', 'star', 'icons']});
+	html = converter.makeHtml(Resume);
 
-	var lib = [
-		{ name: 'Backbone.js', url: 'http://documentcloud.github.io/backbone/'},
-		{ name: 'AngularJS', url: 'https://angularjs.org/'},
-		{ name: 'jQuery', url: 'http://jquery.com/'},
-		{ name: 'Prototype', url: 'http://www.prototypejs.org/'},
-		{ name: 'React', url: 'http://facebook.github.io/react/'},
-		{ name: 'Ember', url: 'http://emberjs.com/'},
-		{ name: 'Knockout.js', url: 'http://knockoutjs.com/'},
-		{ name: 'Dojo', url: 'http://dojotoolkit.org/'},
-		{ name: 'Mootools', url: 'http://mootools.net/'},
-		{ name: 'Underscore', url: 'http://documentcloud.github.io/underscore/'},
-		{ name: 'Lodash', url: 'http://lodash.com/'},
-		{ name: 'Moment', url: 'http://momentjs.com/'},
-		{ name: 'Express', url: 'http://expressjs.com/'},
-		{ name: 'Koa', url: 'http://koajs.com/'}
-	];
-
-	var ExampleComponent = React.createClass({
+	ExampleComponent = React.createClass({
 		onButtonClicked: function () {
-			var doc = new jsPDF();
-			var elementHandler = {
-				'#main_content': function (element, renderer) {
+			var doc, elementHandler, source;
+			doc = new jsPDF();
+			elementHandler = {
+				'#main_content': function () {
 					return true;
 				}
 			};
-			var source = $('#main_content').html();
-			doc.fromHTML(
-				source,
-				15,
-				15,
+			source = $('#main_content').html();
+			doc.fromHTML(source, 15, 15,
 				{
-					'width': 180,'elementHandlers': elementHandler
+					'width': 180, 'elementHandlers': elementHandler
 				});
-			doc.save("dataurlnewwindow.pdf");
+			doc.save("resume.pdf");
 		},
 		getInitialState: function () {
 			return {searchString: ''};
@@ -56,8 +39,12 @@ define([
 			this.setState({searchString: e.target.value});
 		},
 		render: function () {
-			var libraries = lib,
-				searchString = this.state.searchString.trim().toLowerCase();
+			var libraries, searchString;
+			libraries = [];
+			$.each($(html).find('ol li'), function (index, list) {
+				libraries.push({name: $(list).text()});
+			});
+			searchString = this.state.searchString.trim().toLowerCase();
 
 			if (searchString.length > 0) {
 				libraries = libraries.filter(function (l) {
@@ -67,19 +54,22 @@ define([
 
 			return (
 				<div className="pure-g">
-					<div className="pure-u-3 pure-u-md-2-3 example" >
-						<input type="button" className="pure-button pure-button-primary" onClick={this.onButtonClicked} value="export as pdf"/>
-						<input type='text' value={this.state.searchString} onChange={this.handleChange} placeholder='Type here'/>
-						<ul>
+					<div className="pure-u-3 pure-u-md-2-3 example">
+						<input type="button" className="pure-button pure-button-primary" onClick={this.onButtonClicked}
+						       value="export as pdf"/>
+						Search:
+						<input type='text' value={this.state.searchString} onChange={this.handleChange}
+						       placeholder='Type here'/>
+
+						<p><strong>Keywords</strong>:
 							{
 								libraries.map(function (l) {
-									return <li>{l.name}
-										<a href={l.url}>{l.url}</a>
-									</li>;
+									return <span>{l.name},</span>;
 								})
 							}
-						</ul>
-						<div dangerouslySetInnerHTML={{ __html: converter.makeHtml(Resume) }}/>
+						</p>
+
+						<div dangerouslySetInnerHTML={{ __html: html }}/>
 					</div>
 				</div>
 			);
