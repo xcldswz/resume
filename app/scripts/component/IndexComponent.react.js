bc29a4e695c4d568d6f62cc3363641ec66a00f19
Fengda HUANG <h@phodal.com>
2015-04-09 08:55:42 +0800

    add hidden others function

--- app/scripts/component/IndexComponent.react.js
+++ app/scripts/component/IndexComponent.react.js
@@ -3,6 +3,7 @@
 define([
 	'react',
 	'showdown',
+	'jquery',
 	'text!../data/example.md',
 	'jsx!component/SearchComponent.react',
 	'jsx!component/ExportPDFComponent.react',
@@ -10,23 +11,33 @@ define([
 	'prettify',
 	'star',
 	'icons'
-], function (React, Showdown, example, SearchComponent, ExportPDFComponent) {
+], function (React, Showdown, $, example, SearchComponent, ExportPDFComponent) {
+	var IndexComponent;
 	var converter = new Showdown.converter({ extensions: ['table', 'prettify', 'star', 'icons'] });
 
-	var IndexComponent = React.createClass({
+	IndexComponent = React.createClass({
 		getInitialState: function () {
 			return {value: example};
 		},
 		handleChange: function () {
 			this.setState({value: this.refs.textarea.getDOMNode().value});
 		},
+		hiddenOthers: function () {
+			$('.input').hide();
+			$('.top').hide();
+			$('.header').hide();
+		},
 		render: function () {
 			var html = converter.makeHtml(this.state.value);
 			return (
 				<div>
 					<div className="pure-u-1 top">
-						<ExportPDFComponent id={'resume'} />
-						<SearchComponent html={html} />
+						<button className="pure-button pure-button-primary" onClick={this.hiddenOthers}>
+							<i className="fa fa-arrow-left"></i> Only Output
+						</button>
+						<span></span>
+						<ExportPDFComponent id={'resume'}/>
+						<SearchComponent html={html}/>
 					</div>
 					<div className="pure-u-1 pure-u-md-1-2 input">
 						<h3>Input</h3>
@@ -34,7 +45,8 @@ define([
 					</div>
 					<div className="pure-u-1 pure-u-md-1-2 output">
 						<h3>Output</h3>
-						<div className="resume" id="resume" dangerouslySetInnerHTML={{ __html:  html }} />
+
+						<div className="resume" id="resume" dangerouslySetInnerHTML={{ __html:  html }}/>
 					</div>
 				</div>
 			);
